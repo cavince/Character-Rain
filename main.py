89824d20458c75bbe214d496c6592c8387d2f49e
Charles Vincent <50090437+cavince@users.noreply.github.com>
2019-04-29 03:44:39 +0000

    Outlining thoughts

---
+++ main.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env Python3
+
+main():
+    print("Input string: ")
+    inputString = input()
+    while !isinstance(inputString, str):
+        print("Try again!")
+        print(Input string: ")
+        inputString = input()
+    stringLength = len(inputString)
+    
+    What I want to do: Separate characters into array of tuples (triples?), with (char, rand) (char, initpos, rand)?
+    Then sort array based on rand
+    output = inputString + "\n"
+    while i < stringLength:
+        
