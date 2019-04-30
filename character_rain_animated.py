64882dbbff1731b4a1f4c18562e00813a29ed9fa
Charles Vincent <50090437+cavince@users.noreply.github.com>
2019-04-30 21:01:48 +0000

    Update character_rain_animated.py

--- character_rain_animated.py
+++ character_rain_animated.py
@@ -8,52 +8,52 @@ import tkinter as tk
 charlist = []
 
 def assign_random_values(input_string, string_length):
-	charlist = []
-	for c in input_string:
-		vertical_offset = random.randrange(string_length)
-		charlist.append([c, vertical_offset])
-	return charlist
+    charlist = []
+    for c in input_string:
+        vertical_offset = random.randrange(string_length)
+        charlist.append([c, vertical_offset])
+    return charlist
 	
 def create_output_string(charlist, string_length):
-	output = ''
-	for i in range(string_length):
-		for x in charlist:
-			char = x[0]
-			offset = x[1]
-			if offset == i:
-				output += char
-			else:
-				output += ' '
-		output += '\n'
-	return output
+    output = ''
+    for i in range(string_length):
+        for x in charlist:
+            char = x[0]
+            offset = x[1]
+            if offset == i:
+                output += char
+            else:
+                output += ' '
+            output += '\n'
+    return output
 
 def animation(label, string_length):
-	def animate():
-		global charlist
-		for x in charlist:
-			x[1] += 1
-			x[1] %= string_length
-		output = create_output_string(charlist, string_length)
-		label.config(text=output)
-		label.after(10, animate)
-	animate()
+    def animate():
+        global charlist
+        for x in charlist:
+            x[1] += 1
+            x[1] %= string_length
+        output = create_output_string(charlist, string_length)
+        label.config(text=output)
+        label.after(10, animate)
+    animate()
 
 def main():
-	input_string = input('Input string: ')
-	string_length = len(input_string)
+    input_string = input('Input string: ')
+    string_length = len(input_string)
 	
-	root = tk.Tk()
-	root.title('Character Rain')
-	label = tk.Label(root, text='')
+    root = tk.Tk()
+    root.title('Character Rain')
+    label = tk.Label(root, text='')
 	
-	global charlist
-	charlist = assign_random_values(input_string, string_length)
-	output = create_output_string(charlist, string_length)
+    global charlist
+    charlist = assign_random_values(input_string, string_length)
+    output = create_output_string(charlist, string_length)
 	
-	label.config(text=output)
-	label.pack()
-	animation(label, string_length)
-	root.mainloop()
+    label.config(text=output)
+    label.pack()
+    animation(label, string_length)
+    root.mainloop()
 	
 if __name__ == '__main__':
-	main()
+    main()
